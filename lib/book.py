bbaa010eb84eef57095264dac9dbd2de72293f77
Rayken20 <rachael92njoki@gmail.com>
2024-03-09 10:56:10 +0300

    complete

--- lib/book.py
+++ lib/book.py
@@ -2,15 +2,22 @@ class Book:
     """Represents a book with a title and page count."""
 
     def __init__(self, title: str, page_count: int):
-    
-        if not isinstance(page_count, int):
-           print ("page_count must be an integer\n")
-        elif page_count < 1:
-            print ("page_count must be an integer\n")
-        else:        
-             self.title = title
-             self.page_count = page_count
+        self.title = title
+        self._page_count = None  # Initialize _page_count attribute
+
+        # Set page_count using property setter
+        self.page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+        else:
+            self._page_count = value
 
     def turn_page(self):
-    
         print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -2,13 +2,23 @@ class Shoe:
     """Represents a shoe with a brand and size."""
 
     def __init__(self, brand: str, size: int):
-        if not isinstance(size, int):
+        self.brand = brand
+        self._size = None  # Initialize _size attribute
+
+        # Set size using property setter
+        self.size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             print("size must be an integer")
-    
         else:
-            self.brand = brand
-            self.size = size
+            self._size = value
 
     def cobble(self):
-        print("Your shoe is as good as new!\n")
+        print("Your shoe is as good as new!")
         self.condition = "New"
