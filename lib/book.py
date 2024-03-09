9572d7d7635bb638689ed2843afade4880b4070e
Rayken20 <rachael92njoki@gmail.com>
2024-03-09 09:21:06 +0300

    almost done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env python3
-
 class Book:
-    pass
+    """Represents a book with a title and page count."""
+
+    def __init__(self, title: str, page_count: int):
     
-        
+        if not isinstance(page_count, int):
+           print ("page_count must be an integer\n")
+        elif page_count < 1:
+            print ("page_count must be an integer\n")
+        else:        
+             self.title = title
+             self.page_count = page_count
+
+    def turn_page(self):
+    
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    """Represents a shoe with a brand and size."""
+
+    def __init__(self, brand: str, size: int):
+        if not isinstance(size, int):
+            print("size must be an integer")
+    
+        else:
+            self.brand = brand
+            self.size = size
+
+    def cobble(self):
+        print("Your shoe is as good as new!\n")
+        self.condition = "New"
